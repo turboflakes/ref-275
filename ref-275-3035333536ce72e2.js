11ebedec67bb3ca7f7e7f5e9b92ad3394d4427c3
paulo <paulo.rmartins@gmail.com>
2023-10-01 16:54:24 +0100

    push latest release

--- ref-275-3035333536ce72e2.js
+++ ref-275-3035333536ce72e2.js
@@ -997,7 +997,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('ref-275-4112c0dbf3893cb7_bg.wasm', import.meta.url);
+        input = new URL('ref-275-3035333536ce72e2_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
